19033eec168af81e3e07ede3ba77e2cf873dfe24
aidenkitch <aidenkitchen@gmail.com>
2026-03-02 18:36:04 -0800

    simple addition of rt ./config/db.js;

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
+import "./config/db.js";
 
 app.listen(env.port, () => {
   console.log(`EcoRoute backend running on http://localhost:${env.port}`);
